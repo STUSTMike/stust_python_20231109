ef3904082fd54c26b5aa156009828158df0ced1b
STUSTMike <112836953+STUSTMike@users.noreply.github.com>
2023-11-23 11:59:00 +0800

    Update Features.py

---
+++ 1123/Features.py
@@ -0,0 +1,13 @@
+class duel:
+
+    
+    def __init__(sefl,name,age,attack,shield,HP,MP):
+        sefl.name=name
+        sefl.age=age
+        sefl.attack=attack
+        sefl.shield=shield
+        sefl.HP=HP
+        sefl.MP=MP
+
+
+    

9a3e10d2cec33152ed8616b45353d3c08ea87520
STUSTMike <112836953+STUSTMike@users.noreply.github.com>
2023-11-30 19:43:26 +0800

    Update Features.py

--- 1123/Features.py
+++ 1123/Features.py
@@ -8,6 +8,15 @@ class duel:
         sefl.shield=shield
         sefl.HP=HP
         sefl.MP=MP
+        print(name,age,attack,HP,MP)
+class PK(duel):
+        def __init__(sefl, name, age, attack, shield, HP, MP):
+            super().__init__(name, age, attack, shield, HP, MP)    
+            while(HP!=0):
+                HP=HP-sefl.attack
+                print(name,age,attack,HP,MP)
+x=duel("騎士",10,20,15,100,5)
+p=PK("女巫",20,10,10,200,20)
 
 
     

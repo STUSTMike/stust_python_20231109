0464743d27e74c45fcfb55287d2ffec037807651
STUSTMike <112836953+STUSTMike@users.noreply.github.com>
2023-12-02 00:58:31 +0800

    update Features.py

--- 1123/Features.py
+++ 1123/Features.py
@@ -1,6 +1,4 @@
 class duel:
-
-    
     def __init__(sefl,name,age,attack,shield,HP,MP):
         sefl.name=name
         sefl.age=age
@@ -9,14 +7,34 @@ class duel:
         sefl.HP=HP
         sefl.MP=MP
         print(name,age,attack,HP,MP)
+
 class PK(duel):
         def __init__(sefl, name, age, attack, shield, HP, MP):
-            super().__init__(name, age, attack, shield, HP, MP)    
-            while(HP!=0):
-                HP=HP-sefl.attack
+            super().__init__(name, age, attack, shield, HP, MP) 
+            while(HP>0 or x.HP>0):
+                # print("第{}回合".format(i))
+                HP=HP-x.attack
                 print(name,age,attack,HP,MP)
-x=duel("騎士",10,20,15,100,5)
+                x.HP=x.HP-attack
+                print(x.name,x.age,x.attack,x.HP,x.MP)
+                if(HP<=0 or x.HP<=0):
+                    break
+              
+            if(x.HP<=0):
+                print("{}死了".format(x.name))
+            else:
+                print("{}死了".format(name))
+
+class P1(duel):
+     pass
+
+class P2(duel):
+     pass
+
+x=duel("苦力怕",10,50,10,100,5)
 p=PK("女巫",20,10,10,200,20)
+
+
 
 
     
